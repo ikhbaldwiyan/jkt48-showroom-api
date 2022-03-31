96b548cd47ad1877fb70ac3a46611abe2c7ff9fe
Ikhbal Dwiyantoro <ikhbal.d@smt-id.com>
2022-03-31 23:27:28 +0700

    add new member endpoint

--- routes/roomRoute.js
+++ routes/roomRoute.js
@@ -7,5 +7,6 @@ router.get('/onlives', Rooms.getRoomLive)
 router.get('/profile/:roomId', Rooms.getProfile)
 router.get('/schedule/:roomId', Rooms.getNextLive)
 router.get('/total-rank/:roomId', Rooms.getTotalRank)
+router.get('/academy', Rooms.getNewMember)
 
 module.exports = router;
