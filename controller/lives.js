cbc4cd5427915e62bdf912d845ab70bfafda64f1
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2023-01-23 20:47:09 +0700

    fix indent spacing code

--- controller/lives.js
+++ controller/lives.js
@@ -2,70 +2,66 @@ const { LIVE, ROOM } = require("../utils/api");
 const fetchService = require("../utils/fetchService");
 
 const Lives = {
-    getStreamUrl: async (req, res) => {
-        try {
-            const roomId = req.params.roomId;
-            const url = `${LIVE}/streaming_url?room_id=${roomId}`
-            const response = await fetchService(url, res);
-            const streamUrl = response.data.streaming_url_list;
-            
-            res.send(streamUrl);
+  getStreamUrl: async (req, res) => {
+    try {
+      const roomId = req.params.roomId;
+      const url = `${LIVE}/streaming_url?room_id=${roomId}`;
+      const response = await fetchService(url, res);
+      const streamUrl = response.data.streaming_url_list;
 
-        } catch (error) {
-            return error
-        }
-    },
+      res.send(streamUrl);
+    } catch (error) {
+      return error;
+    }
+  },
 
-    getComments: async (req, res) => {
-        try {
-            const roomId = req.params.roomId;
-            const url = `${LIVE}/comment_log?room_id=${roomId}`
-            const response = await fetchService(url, res);
-            const comments = response.data.comment_log;
-            
-            res.send(comments);
+  getComments: async (req, res) => {
+    try {
+      const roomId = req.params.roomId;
+      const url = `${LIVE}/comment_log?room_id=${roomId}`;
+      const response = await fetchService(url, res);
+      const comments = response.data.comment_log;
 
-        } catch (error) {
-            return error
-        }
-    },
+      res.send(comments);
+    } catch (error) {
+      return error;
+    }
+  },
 
-    getTitle: async (req, res) => {
-        try {
-            const roomId = req.params.roomId;
-            const profileUrl = `${ROOM}/profile?room_id=${roomId}`;
-            const profileApi = await fetchService(profileUrl, res);
-            const profile = profileApi.data;
+  getTitle: async (req, res) => {
+    try {
+      const roomId = req.params.roomId;
+      const profileUrl = `${ROOM}/profile?room_id=${roomId}`;
+      const profileApi = await fetchService(profileUrl, res);
+      const profile = profileApi.data;
 
-            const titleUrl = `${LIVE}/telop?room_id=${roomId}`;
-            const titleApi = await fetchService(titleUrl, res);
-            const title = titleApi.data.telop;
+      const titleUrl = `${LIVE}/telop?room_id=${roomId}`;
+      const titleApi = await fetchService(titleUrl, res);
+      const title = titleApi.data.telop;
 
-            // Destrurct response profile and title
-            const profileData = (profile, title) => {
-                const name = `${profile.room_name}is Live on JKT48 SHOWROOM`
-                const share = `https://twitter.com/intent/tweet?text=${name}%0ahttps://jkt48-showroom.vercel.app/live-stream/${roomId}`
-                return {
-                    roomId: profile.room_id,
-                    room_name: profile.room_name,
-                    room_url_key: profile.room_url_key,
-                    title: title,
-                    views: profile.view_num,
-                    is_onlive: profile.is_onlive,
-                    current_live_started_at: profile.current_live_started_at,
-                    share_url_live: profile.share_url_live,
-                    share_url_local: share
-                }
-            }
-            const data = profileData(profile, title)
-            
-            res.send(data);
+      // Destrurct response profile and title
+      const profileData = (profile, title) => {
+        const name = `${profile.room_name}is Live on JKT48 SHOWROOM`;
+        const share = `https://twitter.com/intent/tweet?text=${name}%0ahttps://jkt48-showroom.vercel.app/live-stream/${roomId}`;
+        return {
+          roomId: profile.room_id,
+          room_name: profile.room_name,
+          room_url_key: profile.room_url_key,
+          title: title,
+          views: profile.view_num,
+          is_onlive: profile.is_onlive,
+          current_live_started_at: profile.current_live_started_at,
+          share_url_live: profile.share_url_live,
+          share_url_local: share,
+        };
+      };
+      const data = profileData(profile, title);
 
-        } catch (error) {
-            return error
-        }
-    },
-  
-}
+      res.send(data);
+    } catch (error) {
+      return error;
+    }
+  },
+};
 
 module.exports = Lives;
--- controller/rooms.js
+++ controller/rooms.js
@@ -2,177 +2,211 @@ const { HOME, LIVE, ROOM } = require("../utils/api");
 const fetchService = require("../utils/fetchService");
 
 const Rooms = {
-    getRoomList: async (req, res) => {
-        let roomList = []
-        try {
-            const response = await fetchService(HOME, res);
-            const rooms = response.data
+  getRoomList: async (req, res) => {
+    let roomList = [];
+    try {
+      const response = await fetchService(HOME, res);
+      const rooms = response.data;
 
-            for (let i = 0; i < rooms.length; i++) {
-                const index = rooms[i];
-                if (index.name.includes("JKT48") && !index.url_key.includes('JKT48_Eve') && !index.url_key.includes('JKT48_Ariel') && !index.url_key.includes('JKT48_Anin') && !index.url_key.includes('JKT48_Cindy')) 
-                {
-                    roomList.push(index)
-                }
-            }
-            
-            res.send(roomList);
-
-        } catch (error) {
-            return error
+      for (let i = 0; i < rooms.length; i++) {
+        const index = rooms[i];
+        if (
+          index.name.includes("JKT48") &&
+          !index.url_key.includes("JKT48_Eve") &&
+          !index.url_key.includes("JKT48_Ariel") &&
+          !index.url_key.includes("JKT48_Anin") &&
+          !index.url_key.includes("JKT48_Cindy")
+        ) {
+          roomList.push(index);
         }
-    },
+      }
 
-    getRoomLive: async (req, res) => {
-        try {
-            let onLive = [];
-            let roomIsLive = [];
-            let findSisca = [];
+      res.send(roomList);
+    } catch (error) {
+      return error;
+    }
+  },
 
-            const response = await fetchService(`${LIVE}/onlives`, res);
-            const data = response.data.onlives
+  getRoomLive: async (req, res) => {
+    try {
+      let onLive = [];
+      let roomIsLive = [];
+      let findSisca = [];
 
-            // Find Member Live
-            for (let i = 0; i < data.length; i++) {
-                const index = data[i];
-                if (index.genre_name === "Idol") {
-                    onLive.push(index)
-                }
-            }
+      const response = await fetchService(`${LIVE}/onlives`, res);
+      const data = response.data.onlives;
 
-            if (onLive.length) {
-                const roomLive = onLive[0].lives;
-            
-                roomLive.forEach(item => {
-                    if (item.room_url_key.includes('JKT48')) {
-                        roomIsLive.push(item)
-                    }
-                });
-            }
-
-            // Find Sisca
-            for (let i = 0; i < data.length; i++) {
-                const index = data[i];
-                if (index.genre_name === "Music") {
-                    findSisca.push(index)
-                }
-            }
-
-            if (findSisca.length) {
-                const siscaLive = findSisca[0].lives;
-            
-                siscaLive.forEach(item => {
-                    if (item.room_url_key.includes('JKT48')) {
-                        roomIsLive.push(item)
-                    }
-                });
-            }
-
-            if (roomIsLive.length === 0) {
-                res.send({
-                    message : 'Member Not Live',
-                    is_live: false
-                });
-            }
-
-            res.send(roomIsLive);
-
-        } catch (error) {
-            return error
+      // Find Member Live
+      for (let i = 0; i < data.length; i++) {
+        const index = data[i];
+        if (index.genre_name === "Idol") {
+          onLive.push(index);
         }
-    },
+      }
 
-    getProfile: async (req, res) => {
-        try {
-            const { roomId } = req.params
-            const getProfile = await fetchService(`${ROOM}/profile?room_id=${roomId}`, res)
-            const profile = getProfile.data
+      if (onLive.length) {
+        const roomLive = onLive[0].lives;
 
-            res.send(profile)
+        roomLive.forEach((item) => {
+          if (item.room_url_key.includes("JKT48")) {
+            roomIsLive.push(item);
+          }
+        });
+      }
 
-        } catch (error) {
-            res.send(error)
+      // Find Sisca
+      for (let i = 0; i < data.length; i++) {
+        const index = data[i];
+        if (index.genre_name === "Music") {
+          findSisca.push(index);
         }
-    },
+      }
 
-    getNextLive: async (req, res) => {
-        try {
-            const { roomId } = req.params
-            const getNextLive = await fetchService(`${ROOM}/next_live?room_id=${roomId}`, res)
-            const nextLive = getNextLive.data
+      if (findSisca.length) {
+        const siscaLive = findSisca[0].lives;
 
-            res.send(nextLive)
+        siscaLive.forEach((item) => {
+          if (item.room_url_key.includes("JKT48")) {
+            roomIsLive.push(item);
+          }
+        });
+      }
 
-        } catch (error) {
-            res.send(error)
-        }
-    },
+      if (roomIsLive.length === 0) {
+        res.send({
+          message: "Member Not Live",
+          is_live: false,
+        });
+      }
 
-    getTotalRank: async (req, res) => {
-        try {
-            const { roomId } = req.params
-            const getTotalRank = await fetchService(`${LIVE}/summary_ranking?room_id=${roomId}`, res)
-            const totalRank = getTotalRank.data.ranking
+      res.send(roomIsLive);
+    } catch (error) {
+      return error;
+    }
+  },
 
-            res.send(totalRank);
+  getProfile: async (req, res) => {
+    try {
+      const { roomId } = req.params;
+      const getProfile = await fetchService(
+        `${ROOM}/profile?room_id=${roomId}`,
+        res
+      );
+      const profile = getProfile.data;
 
-        } catch (error) {
-            res.send(error)
-        }
-    },
+      res.send(profile);
+    } catch (error) {
+      res.send(error);
+    }
+  },
 
-    getNewMember: async(req, res) => {
-        try {
-            let newMember = [];
+  getNextLive: async (req, res) => {
+    try {
+      const { roomId } = req.params;
+      const getNextLive = await fetchService(
+        `${ROOM}/next_live?room_id=${roomId}`,
+        res
+      );
+      const nextLive = getNextLive.data;
 
-            const ACADEMY = {
-                amanda: '400710',
-                lia: '400713',
-                giselle : '400712',
-                callie: '400714',
-                ela: '400715',
-                indira:  '400716',
-                lyn: '400717',
-                raisha:  '400718'
-            }
+      res.send(nextLive);
+    } catch (error) {
+      res.send(error);
+    }
+  },
 
-            const amanda = await fetchService(`${ROOM}/profile?room_id=${ACADEMY.amanda}`, res)
-            const lia = await fetchService(`${ROOM}/profile?room_id=${ACADEMY.lia}`, res)
-            const giselle = await fetchService(`${ROOM}/profile?room_id=${ACADEMY.giselle}`, res)
-            const callie = await fetchService(`${ROOM}/profile?room_id=${ACADEMY.callie}`, res)
-            const ela = await fetchService(`${ROOM}/profile?room_id=${ACADEMY.ela}`, res)
-            const indira = await fetchService(`${ROOM}/profile?room_id=${ACADEMY.indira}`, res)
-            const lyn = await fetchService(`${ROOM}/profile?room_id=${ACADEMY.lyn}`, res)
-            const raisha = await fetchService(`${ROOM}/profile?room_id=${ACADEMY.raisha}`, res)
+  getTotalRank: async (req, res) => {
+    try {
+      const { roomId } = req.params;
+      const getTotalRank = await fetchService(
+        `${LIVE}/summary_ranking?room_id=${roomId}`,
+        res
+      );
+      const totalRank = getTotalRank.data.ranking;
 
-            newMember.push(amanda.data)
-            newMember.push(callie.data)
-            newMember.push(ela.data)
-            newMember.push(giselle.data)
-            newMember.push(lia.data)
-            newMember.push(indira.data)
-            newMember.push(lyn.data)
-            newMember.push(raisha.data)
-            
-            res.send(newMember)
-        } catch (error) {
-            res.send(error)
-        }
-    },
+      res.send(totalRank);
+    } catch (error) {
+      res.send(error);
+    }
+  },
 
-    getFanLetter: async (req, res) => {
-        try {
-            const { roomId } = req.params
-            const getFanLetter = await fetchService(`${ROOM}/recommend_comments?room_id=${roomId}`, res)
-            const fanLetter = getFanLetter.data.recommend_comments
+  getNewMember: async (req, res) => {
+    try {
+      let newMember = [];
 
-            res.send(fanLetter);
+      const ACADEMY = {
+        amanda: "400710",
+        lia: "400713",
+        giselle: "400712",
+        callie: "400714",
+        ela: "400715",
+        indira: "400716",
+        lyn: "400717",
+        raisha: "400718",
+      };
 
-        } catch (error) {
-            res.send(error)
-        }
-    },
+      const amanda = await fetchService(
+        `${ROOM}/profile?room_id=${ACADEMY.amanda}`,
+        res
+      );
+      const lia = await fetchService(
+        `${ROOM}/profile?room_id=${ACADEMY.lia}`,
+        res
+      );
+      const giselle = await fetchService(
+        `${ROOM}/profile?room_id=${ACADEMY.giselle}`,
+        res
+      );
+      const callie = await fetchService(
+        `${ROOM}/profile?room_id=${ACADEMY.callie}`,
+        res
+      );
+      const ela = await fetchService(
+        `${ROOM}/profile?room_id=${ACADEMY.ela}`,
+        res
+      );
+      const indira = await fetchService(
+        `${ROOM}/profile?room_id=${ACADEMY.indira}`,
+        res
+      );
+      const lyn = await fetchService(
+        `${ROOM}/profile?room_id=${ACADEMY.lyn}`,
+        res
+      );
+      const raisha = await fetchService(
+        `${ROOM}/profile?room_id=${ACADEMY.raisha}`,
+        res
+      );
 
-}
+      newMember.push(amanda.data);
+      newMember.push(callie.data);
+      newMember.push(ela.data);
+      newMember.push(giselle.data);
+      newMember.push(lia.data);
+      newMember.push(indira.data);
+      newMember.push(lyn.data);
+      newMember.push(raisha.data);
+
+      res.send(newMember);
+    } catch (error) {
+      res.send(error);
+    }
+  },
+
+  getFanLetter: async (req, res) => {
+    try {
+      const { roomId } = req.params;
+      const getFanLetter = await fetchService(
+        `${ROOM}/recommend_comments?room_id=${roomId}`,
+        res
+      );
+      const fanLetter = getFanLetter.data.recommend_comments;
+
+      res.send(fanLetter);
+    } catch (error) {
+      res.send(error);
+    }
+  },
+};
 
 module.exports = Rooms;
