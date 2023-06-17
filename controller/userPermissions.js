c8094337644b10880a179670eaa3be4802f44aee
ikhbaldwiyan <ikhbaldwiyan@gmail.com>
2023-06-18 01:14:15 +0700

    handle user id is already used

--- controller/userPermissions.js
+++ controller/userPermissions.js
@@ -16,7 +16,9 @@ exports.createUser = async (req, res) => {
     const savedUser = await newUser.save();
     res.status(201).json({ message: 'User created successfully', user: savedUser });
   } catch (error) {
-    // console.error('Error creating user:', error);
+    if (error.code === 11000) {
+      res.status(409).json({ error: 'User ID already taken' });
+    } 
     res.status(500).json({ error: 'Failed to create user' });
   }
 };
